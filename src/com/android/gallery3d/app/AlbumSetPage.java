3dd33282de2941cd4487753a1eafa388974473e3
Wen-Hao Chang <wenhao.chang@gigabyte.com>
2013-04-01 16:37:38 +0800

    Gallery: Remove "Settings" item from gallery menu.
    Open "Gallery -> menu -> Settings", it is empty.
    
    And, GallerySettings.java is empty, too.
    
    Category: gallery
    Bug: None
    
    Change-Id: I797d58c4c1340b7ad57e853a2d8fefd34b0e8854

--- src/com/android/gallery3d/app/AlbumSetPage.java
+++ src/com/android/gallery3d/app/AlbumSetPage.java
@@ -499,10 +499,14 @@ public class AlbumSetPage extends ActivityState implements
                 PicasaSource.requestSync(activity);
                 return true;
             }
+
+            /*
             case R.id.action_settings: {
                 activity.startActivity(new Intent(activity, GallerySettings.class));
                 return true;
             }
+            */
+
             default:
                 return false;
         }
