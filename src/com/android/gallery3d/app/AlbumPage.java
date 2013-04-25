3a2a0a8ce31d1785996ffad4252e4dce673566b8
kaiyiz <kaiyiz@codeaurora.org>
2013-04-25 00:03:49 -0400

    Gallery2: keep selection mode when deSelectAll
    Origin code will call leaveSelectionMode function in
    deSelectAll and will not keep in selected status on
    this situation.
    
    Add DESELECT_ALL_MODE in AlbumPage and
    AlbumSetPage to make stay in selection mode even if
    choose deSelectAll.
    
    CRs-Fixed: 342469
    
    Change-Id: Ieb7e38711e7380b55a15f8d4de8fc2fdadc3eaaf

--- src/com/android/gallery3d/app/AlbumPage.java
+++ src/com/android/gallery3d/app/AlbumPage.java
@@ -675,6 +675,11 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
                 mRootPane.invalidate();
                 break;
             }
+            case SelectionManager.DESELECT_ALL_MODE: {
+                mActionModeHandler.updateSupportedOperation();
+                mRootPane.invalidate();
+                break;
+            }
         }
     }
 
