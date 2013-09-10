46ec07b3f7c3803ce6cd29c37656f163308522b1
Qu Tao <taoq@codeaurora.org>
2013-09-10 04:40:50 -0700

    Revert "Gallery2: support loop/single video play mode"
    This reverts commit 0fecc2824386ba6f916078bf7216fd2f8483d81f.
    
    Change-Id: I4530e5fb225eafeb86ce02e1ecb42380156873c7

--- src/com/qcom/gallery3d/ext/IMovieItem.java
+++ src/com/qcom/gallery3d/ext/IMovieItem.java
@@ -1,7 +1,6 @@
-package org.codeaurora.gallery3d.ext;
+package com.qcom.gallery3d.ext;
 
 import android.net.Uri;
-
 /**
  * Movie info class
  */
@@ -10,39 +9,34 @@ public interface IMovieItem {
      * @return movie Uri, it's may be not the original Uri.
      */
     Uri getUri();
-
     /**
+     * 
      * @return MIME type of video
      */
     String getMimeType();
-
     /**
+     * 
      * @return title of video
      */
     String getTitle();
-
     /**
+     * 
      * @return whether error occured or not.
      */
     boolean getError();
 
     /**
      * set title of video
-     *
      * @param title
      */
     void setTitle(String title);
-
     /**
      * set video Uri
-     *
      * @param uri
      */
     void setUri(Uri uri);
-
     /**
      * Set MIME type of video
-     *
      * @param mimeType
      */
     void setMimeType(String mimeType);
@@ -51,15 +45,13 @@ public interface IMovieItem {
      * Set error occured flag
      */
     void setError();
-
     /**
+     * 
      * @return return original Uri of video.
      */
     Uri getOriginalUri();
-
     /**
      * Set video original Uri.
-     *
      * @param uri
      */
     void setOriginalUri(Uri uri);
