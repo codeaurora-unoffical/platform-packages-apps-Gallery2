2cf227edd71c5bd70723da3f916b1e5115ab6165
lubiny <lubiny@codeaurora.org>
2013-10-11 16:40:51 +0800

    Gallery2: handle "Resume video" dialog when activity pauses and resumes
    - When "Resume video" dialog is visible, MoviePlayer will not save
      bookmark or resume playing.
    
    Change-Id: I9b6aca7cd2d8d098a141c574eba60700d9d33e75
    CRs-Fixed: 530287

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnDismissListener;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
@@ -99,6 +100,9 @@ public class MoviePlayer implements
 
     // If the time bar is visible.
     private boolean mShowing;
+
+    // If the resume dialog is visible
+    private boolean mResumeDialogVisible;
 
     private Virtualizer mVirtualizer;
 
@@ -287,6 +291,12 @@ public class MoviePlayer implements
                 onCompletion();
             }
         });
+        builder.setOnDismissListener(new OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                mResumeDialogVisible = false;
+            }
+        });
         builder.setPositiveButton(
                 R.string.resume_playing_resume, new OnClickListener() {
             @Override
@@ -305,19 +315,22 @@ public class MoviePlayer implements
             }
         });
         builder.show();
+        mResumeDialogVisible = true;
     }
 
     public void onPause() {
         mHasPaused = true;
         mHandler.removeCallbacksAndMessages(null);
-        mVideoPosition = mVideoView.getCurrentPosition();
-        mBookmarker.setBookmark(mUri, mVideoPosition, mVideoView.getDuration());
-        mVideoView.suspend();
-        mResumeableTime = System.currentTimeMillis() + RESUMEABLE_TIMEOUT;
+        if (!mResumeDialogVisible) {
+            mVideoPosition = mVideoView.getCurrentPosition();
+            mBookmarker.setBookmark(mUri, mVideoPosition, mVideoView.getDuration());
+            mVideoView.suspend();
+            mResumeableTime = System.currentTimeMillis() + RESUMEABLE_TIMEOUT;
+        }
     }
 
     public void onResume() {
-        if (mHasPaused) {
+        if (mHasPaused && !mResumeDialogVisible) {
             if (mVideoView.canSeekBackward() || mVideoView.canSeekForward()) {
                 mVideoView.seekTo(mVideoPosition);
             }
