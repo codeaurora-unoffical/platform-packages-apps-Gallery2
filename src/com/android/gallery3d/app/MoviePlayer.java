5b9267bee5a7234189a5885e1927e1fdac147d13
kaiyiz <kaiyiz@codeaurora.org>
2015-01-28 11:19:34 +0800

    Gallery2: Can pop-up the server timeout window to let user to choose
    Cannot pop up "Whether continue to play from the previous place or not"window
    
    Add to determine whether the HTTP address for the judge to
    pops-up "Whether continue to play from the previous place or not" window.
    
    Change-Id: I07d1534b0f247f752b37198a66fc85c334543fa6
    CRs-Fixed: 753834

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -1288,7 +1288,7 @@ public class MoviePlayer implements
         // check whether disconnect from server timeout or not.
         // if timeout, return false. otherwise, return true.
         private boolean passDisconnectCheck() {
-            if (!isFullBuffer()) {
+            if (!isFullBuffer() || mStreamingType == STREAMING_HTTP) {
                 // record the time disconnect from server
                 long now = System.currentTimeMillis();
                 if (LOG) {
@@ -1306,7 +1306,7 @@ public class MoviePlayer implements
         }
 
         private void recordDisconnectTime() {
-            if (!isFullBuffer()) {
+            if (!isFullBuffer() || mStreamingType == STREAMING_HTTP) {
                 // record the time disconnect from server
                 mLastDisconnectTime = System.currentTimeMillis();
             }
